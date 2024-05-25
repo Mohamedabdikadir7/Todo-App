7d4c5b20758afe87dab131d1a9bb1addfb8b4d6a
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-25 23:45:05 +0300

    updating the todo app

--- index.js
+++ index.js
@@ -30,17 +30,55 @@ if (input.value === ''){
 
     const list = document.createElement('li')
     list.textContent = input.value;
+    list.classList.add()
     toList.appendChild(list)
-    // list.classList.add()
-    input.value = ''
+   list.classList.add()
+   save()
+    //input.value = ''
  }
+ input.value = ''
 // const list = document.createElement('li')
 //  list.textContent = input.value;
 //  //list.classList.add()
 //  toList.appendChild(list)
-//  input.value = ''
+//input.value = ''
 })
 
 toList.addEventListener("click" , function (e) {
-        e.target.remove()
+        e.target.style.textDecoration = "linethrough"
+        save()
     })
+   
+    //class="list-group-numbered"
+
+ input.addEventListener('keydown', function(e) {
+   if( e.key === 'Enter' && input.value!== ""){
+    const list = document.createElement('li')
+    list.textContent = input.value;
+    toList.appendChild(list)
+   list.classList.add()
+   input.value = '' 
+   save()
+}
+//input.value = '' 
+ })   
+
+ function save() {
+localStorage.setItem("data", toList.innerHTML )
+ }
+
+function read() {
+  toList.innerHTML = localStorage.getItem("data")
+}
+
+ read()
+
+
+// example 
+//  function save1() {
+//     localStorage.item("data",toList.innerhtml)
+//  }
+
+//  function read3() {
+//     toList.innerhtml = localStorage.getItem("data")
+//  }
